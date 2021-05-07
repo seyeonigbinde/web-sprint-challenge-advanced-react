5e1eb8e8493ad1c6e24ab06c8d1a92bc9bcf5afa
Your Name <you@example.com>
2021-05-07 11:41:07 +0100

    useForm Custom Hook Setup

--- src/hooks/useForm.js
+++ src/hooks/useForm.js
@@ -1,1 +1,20 @@
 // write your custom hook here to control your checkout form
+import {useState} from 'react';
+
+const useForm = (key, initialValue) => {
+
+    const [values, setValues] = useState(() => {
+        const item = window.localStorage.getItem(key);
+        return item ? JSON.parse(item) : initialValue;
+      });
+
+  
+    const setValue = value => {
+        setValues(value);
+      window.localStorage.setItem(key, JSON.stringify(value));
+    }
+  
+    return([values, setValue]);
+}
+
+export default useForm;
