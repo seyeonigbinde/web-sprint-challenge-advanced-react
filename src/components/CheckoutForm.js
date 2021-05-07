f59a14b131244417d7a6dc3da890891fbf98e286
Your Name <you@example.com>
2021-05-07 15:52:25 +0100

    form shows success message on submit with form details test passed

--- src/components/CheckoutForm.js
+++ src/components/CheckoutForm.js
@@ -34,6 +34,7 @@ const CheckoutForm = (props) => {
             name="firstName"
             value={values.firstName}
             onChange={handleChanges}
+            placeholder="First Name"
           />
         </label>
         <label>
@@ -42,6 +43,7 @@ const CheckoutForm = (props) => {
             name="lastName"
             value={values.lastName}
             onChange={handleChanges}
+            placeholder="Last Name"
           />
         </label>
         <label>
@@ -50,19 +52,20 @@ const CheckoutForm = (props) => {
             name="address"
             value={values.address}
             onChange={handleChanges}
+            placeholder="Address"
           />
         </label>
         <label>
           City:
-          <input name="city" value={values.city} onChange={handleChanges} />
+          <input name="city" value={values.city} onChange={handleChanges} placeholder="City" />
         </label>
         <label>
           State:
-          <input name="state" value={values.state} onChange={handleChanges} />
+          <input name="state" value={values.state} onChange={handleChanges} placeholder="State" />
         </label>
         <label>
           Zip:
-          <input name="zip" value={values.zip} onChange={handleChanges} />
+          <input name="zip" value={values.zip} onChange={handleChanges} placeholder="Zip" />
         </label>
         <button>Checkout</button>
       </form>
--- src/components/CheckoutForm.test.js
+++ src/components/CheckoutForm.test.js
@@ -1,37 +1,43 @@
 import React from "react";
-import { render, screen, userEvent, waitFor } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from '@testing-library/user-event';
 import CheckoutForm from "./CheckoutForm";
 
 // Write up the two tests here and make sure they are testing what the title shows
-test('renders without errors', ()=>{
-    render(<CheckoutForm/>);
-});
-
 test("form header renders", () => {
+    render(<CheckoutForm/>);
     const header = screen.queryByText(/checkout form/i);
+    expect(header).toBeInTheDocument();
 });
 
-test("form shows success message on submit with form details", () => {
+test("form shows success message on submit with form details", async () => {
     render(<CheckoutForm />);
-    const firstNameInput = screen.getByLabelText(/first Name:/i);
-    userEvent.type(firstNameInput, "Seyet");
-    const lastNameInput = screen.getByLabelText(/last Name:/i);
+    const firstNameInput = screen.getByPlaceholderText(/first Name/i);
+    userEvent.type(firstNameInput, "Seye");
+    expect(firstNameInput).toBeInTheDocument();
+
+    const lastNameInput = screen.getByPlaceholderText(/last Name/i);
     userEvent.type(lastNameInput, "Onigbinde");
-    const addressInput = screen.getByLabelText(/address:/i);
+    expect(lastNameInput).toBeInTheDocument();
+    const addressInput = screen.getByPlaceholderText(/address/i);
     userEvent.type(addressInput, "Nigeria");
-    const cityInput = screen.getByLabelText(/city:/i);
-    userEvent.type(cityInput, "Thank you");
-    const stateInput = screen.getByLabelText(/state:/i);
-    userEvent.type(stateInput, "Thank you");
-    const zipInput = screen.getByLabelText(/zip:/i);
-    userEvent.type(zipInput, "Thank you");
-    const button = screen.getByRole("button", {name:/checkout/i});
+    expect(addressInput).toBeInTheDocument();
+    const cityInput = screen.getByPlaceholderText(/city/i);
+    userEvent.type(cityInput, "Lekki");
+    expect(cityInput).toBeInTheDocument();
+    const stateInput = screen.getByPlaceholderText(/state/i);
+    userEvent.type(stateInput, "Lagos");
+    expect(stateInput).toBeInTheDocument();
+    const zipInput = screen.getByPlaceholderText(/zip/i);
+    userEvent.type(zipInput, "23401");
+    expect(zipInput).toBeInTheDocument();
+    
+    const button = screen.getByRole("button");
     userEvent.click(button);
-        waitFor(async ()=>{
-                const formDisplay = await screen.findByText("Seyet");
-                expect(formDisplay).toBeInTheDocument();
-            });
+        const firstNameText = screen.queryByText(/Seye/i);
+        expect(firstNameText).toBeInTheDocument()
 });
+
 
 
 
